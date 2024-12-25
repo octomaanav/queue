68d31cc6926fe11e56fb4dc911e52515d7ebd30e
octomaanav <maanav.vashist@gmail.com>
2024-12-24 19:23:34 -0500

    login route added

---
+++ src/app/api/auth/login/route.tsx
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const clientId = process.env.AUTOLAB_CLIENT_ID;
+  const redirectUri = process.env.AUTOLAB_REDIRECT_URI
+  const authUrl = `${process.env.AUTOLAB_AUTHORIZE_ENDPOINT}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=user_info user_courses`;
+
+  console.log("Authorization URL:", authUrl); // Debugging: Log the constructed URL
+  return NextResponse.redirect(authUrl);
+}
